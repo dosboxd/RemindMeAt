f5475311047070623ab62a61fd3337518b7c8225
Dosbol Duysekov <dosbol98@gmail.com>
2025-04-22 13:25:02 +0500

    Initial Commit

---
+++ RemindMeAt/RemindMeAtApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeAtApp.swift
+//  RemindMeAt
+//
+//  Created by Dosbol on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeAtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
