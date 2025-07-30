db939859c15b8d75437af4620864597a2112f1a6
Dosbol Duysekov <dosbol98@gmail.com>
2025-07-30 09:50:50 +0500

    Fixes issues with detents, cameraPosition and contentMargins

--- RemindMeAt/ContentView.swift
+++ RemindMeAt/ContentView.swift
@@ -6,12 +6,14 @@ struct ContentView: View {
     @StateObject private var locationService = LocationService()
     @StateObject private var notifyService = NotifyService(notificationCenter: .current())
 
-    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    @State private var position: MapCameraPosition = .userLocation(fallback: .rect(.world))
+    @State private var previousPosition: MapCameraPosition = .userLocation(fallback: .rect(.world))
     @State private var isPresentedListView: Bool = true
 
+    @State private var showingAll: Bool = false
     @AppStorage("satelliteEnabled", store: .standard) private var satelliteEnabled: Bool = false
 
-    @State private var listDetent = PresentationDetent.fraction(0.33)
+    @State private var listDetent = PresentationDetent.height(300)
     @State private var detailsDetent = PresentationDetent.fraction(0.33)
 
     @State var selection: NotificationEntity?
@@ -31,6 +33,7 @@ struct ContentView: View {
                         mapCircle(name: notification.title, center: notification.center, radius: notification.radius)
                     }
                 }
+                .contentMargins(.bottom, 100)
                 .mapStyle(satelliteEnabled ? .hybrid : .standard)
                 VStack {
                     HStack {
@@ -53,6 +56,14 @@ struct ContentView: View {
             .onTapGesture { position in
                 locate(position: position, proxy: proxy, space: .local)
             }
+            .onChange(of: showingAll) { _, newValue in
+                if newValue {
+                    position = .automatic
+                    previousPosition = position
+                    listDetent = .height(100)
+                    selection = nil
+                }
+            }
             .task {
                 locationService.requestAuthorization()
                 await notifyService.requestAuthorization()
@@ -62,7 +73,12 @@ struct ContentView: View {
                 listBottomSheet
                     .sheet(
                         item: $selection,
-                        onDismiss: { withAnimation { self.position = .automatic } }
+                        onDismiss: {
+                            withAnimation {
+                                self.position = previousPosition
+                                listDetent = .height(100)
+                            }
+                        }
                     ) { selection in
                         CreateNotificationView(
                             notification: Binding(
@@ -108,7 +124,7 @@ struct ContentView: View {
         .listStyle(.plain)
         .background(.ultraThickMaterial)
         .presentationBackgroundInteraction(.enabled)
-        .presentationDetents([.large, .fraction(0.33), .fraction(0.15)], selection: $listDetent)
+        .presentationDetents([.large, .height(300), .height(100)], selection: $listDetent)
         .interactiveDismissDisabled()
     }
 
@@ -125,19 +141,22 @@ struct ContentView: View {
         guard let coordinate = proxy.convert(position, from: space) else { return }
         selection = NotificationEntity(id: "", title: "", center: coordinate, notifyOnEntry: true, notifyOnExit: false, radius: 50)
         withAnimation { self.position = .camera(MapCamera(MKMapCamera(lookingAtCenter: coordinate, fromDistance: 1000, pitch: 0, heading: 0))) }
-        listDetent = .fraction(0.33)
+        listDetent = .height(300)
     }
 
     var mapControls: some View {
         VStack {
             Button {
-                position = .userLocation(fallback: .automatic)
+                position = .userLocation(fallback: .rect(.world))
+                previousPosition = position
+                showingAll = false
+                selection = nil
             } label: {
                 Rectangle()
                     .foregroundStyle(.background)
                     .cornerRadius(8)
                     .overlay(
-                        Image(systemName: position == .userLocation(fallback: .automatic) ? "location.fill" : "location")
+                        Image(systemName: position == .userLocation(fallback: .rect(.world)) ? "location.fill" : "location")
                     )
             }
             .frame(width: 44, height: 44)
@@ -151,6 +170,17 @@ struct ContentView: View {
                     .cornerRadius(8)
                     .overlay(
                         Image(systemName: satelliteEnabled ? "globe.americas.fill" : "globe.americas")
+                    )
+            }
+            .frame(width: 44, height: 44)
+            Button {
+                showingAll.toggle()
+            } label: {
+                Rectangle()
+                    .foregroundStyle(.background)
+                    .cornerRadius(8)
+                    .overlay(
+                        Image(systemName: showingAll ? "map.fill" : "map")
                     )
             }
             .frame(width: 44, height: 44)
